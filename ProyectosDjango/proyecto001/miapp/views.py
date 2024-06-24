5d01a13da8aead03dacb90125ba2ceb4d133fab1
Dafne Huertas <nafechg02@gmail.com>
2024-06-24 17:12:15 -0500

    listo semana 13

---
+++ ProyectosDjango/proyecto001/miapp/views.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+
+
+# Create your views here.
+
+
+
+
+def index(request):
+    return render (request, 'index.html')
+
+
+def saludo(request):
+    return render (request, 'saludo.html')
+
+
+def rango(request):
+    a = 10
+    b = 40
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
+
+
+def rango2(request,a=0,b=100):
+    if a>b:
+        return redirect('rango2',a=b, b=a)
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
